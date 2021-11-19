c46cd2f51d8be42c178d15da5438c59277c41d90
Napa Manoj <napmanoj@publicisgroupe.net>
2021-11-19 08:30:31 +0530

    first commit

---
+++ src/main/java/com/azureappservice/appservice/controller/PricingController.java
@@ -0,0 +1,28 @@
+package com.azureappservice.appservice.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.http.HttpStatus;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class PricingController {
+	protected static final Logger log = LoggerFactory.getLogger(PricingController.class);
+
+	@GetMapping(value = "/azureappservice")
+
+	public ResponseEntity<?> getPricingForScreen1() {
+		ResponseEntity<String> response = new ResponseEntity<String>("checking app service", HttpStatus.ACCEPTED);
+
+		return response;
+
+	}
+
+}
